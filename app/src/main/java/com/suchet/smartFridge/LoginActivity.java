74ce27b27c192eb1fea5c2dceb94280aa634a1a4
MatthieusSuchet <matthieu.suchet@outlook.fr>
2025-04-15 22:48:39 -0700

    beginning of LoginActivity

--- app/src/main/java/com/suchet/smartFridge/LoginActivity.java
+++ app/src/main/java/com/suchet/smartFridge/LoginActivity.java
@@ -1,27 +1,70 @@
 package com.suchet.smartFridge;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.LiveData;
 
+import com.suchet.smartFridge.database.entities.User;
 import com.suchet.smartFridge.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.SignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verifyUser();
+            }
         });
+    }
+
+    private void verifyUser() {
+
+        String username = binding.userNameLoginEditText.getText().toString();
+
+        if(username.isEmpty()){
+            toastMaker("Username may not be blank");
+            return;
+        }
+        /*LiveData<User> userObserver = repository.getUserByUserName(username);
+        userObserver.observe(this, user -> {
+            if(user != null){
+                String password = binding.passwordLoginEditText.getText().toString();
+                if(password.equals(user.getPassword())){
+                    Intent intent = MainActivity.mainIntentFactory(getApplicationContext(), user.getId());
+                    startActivity(intent);
+                }else {
+                    toastMaker("Invalid password");
+                    binding.passwordLoginEditText.setSelection(0);
+                }
+            }else {
+                toastMaker(String.format("%s is not a valid username", username));
+                binding.userNameLoginEditText.setSelection(0);
+            }
+        });*/
+    }
+
+    private void toastMaker(String format) {
+        Toast.makeText(this, format, Toast.LENGTH_SHORT).show();
+    }
+
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, LoginActivity.class);
     }
 }
