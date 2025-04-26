af65450e328e965258d57da592f4112908b77274
cstremel98 <cstremel@csumb.edu>
2025-04-26 15:54:43 -0700

    rebase

---
+++ app/src/main/java/com/suchet/smartFridge/database/entities/LogIn.java
@@ -0,0 +1,12 @@
+package com.suchet.smartFridge.database.entities;
+
+public class LogIn {
+    public LogIn() {
+
+    }
+
+    public void logIn(String username, String password) {
+
+    }
+
+}
--- app/src/test/java/com/suchet/smartFridge/ExampleUnitTest.java
+++ app/src/test/java/com/suchet/smartFridge/ExampleUnitTest.java
@@ -4,14 +4,36 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.suchet.smartFridge.database.entities.Food;
+
+import java.time.LocalDate;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    LocalDate dateToday = LocalDate.now();
+    LocalDate dateTomorrow = dateToday.plusDays(1);
+    Food apple = new Food("Apple", dateToday, 3);
+    Food orange = new Food("Orange", dateTomorrow, 2);
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void expiration_isCorrect() {
+        assertNotEquals(apple.getExpirationDate(), orange.getExpirationDate());
+    }
+
+    @Test
+    public void quantity_isCorrect() {
+        assertNotEquals(apple.getQuantity(), orange.getQuantity());
+        orange.setQuantity(3);
+        assertEquals(apple.getQuantity(), orange.getQuantity());
     }
 }
