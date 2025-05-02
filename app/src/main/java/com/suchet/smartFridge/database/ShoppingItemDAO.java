d633d03bb9ba9a2a9341628e55a6287a3760889e
MatthieusSuchet <matthieu.suchet@outlook.fr>
2025-05-02 15:56:05 -0700

    databaseSLDone

---
+++ app/src/main/java/com/suchet/smartFridge/database/ShoppingItemDAO.java
@@ -0,0 +1,27 @@
+package com.suchet.smartFridge.database.daos;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Delete;
+import androidx.room.Update;
+
+import com.suchet.smartFridge.database.entities.ShoppingItem;
+
+import java.util.List;
+
+@Dao
+public interface ShoppingItemDAO {
+
+    @Insert
+    void insert(ShoppingItem item);
+
+    @Update
+    void update(ShoppingItem item);
+
+    @Delete
+    void delete(ShoppingItem item);
+
+    @Query("SELECT * FROM shopping_items WHERE userId = :userId")
+    List<ShoppingItem> getAllForUser(int userId);
+}
--- app/src/main/java/com/suchet/smartFridge/stocks/ShoppingListActivity.java
+++ app/src/main/java/com/suchet/smartFridge/stocks/ShoppingListActivity.java
@@ -11,17 +11,51 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.suchet.smartFridge.R;
+import com.suchet.smartFridge.database.SmartFridgeDatabase;
+import com.suchet.smartFridge.database.entities.ShoppingItem;
+import com.suchet.smartFridge.database.entities.User;
 import com.suchet.smartFridge.databinding.ActivityShoppingListBinding;
 import com.suchet.smartFridge.databinding.ActivityStockBinding;
 
+import java.util.ArrayList;
+
 public class ShoppingListActivity extends AppCompatActivity {
     private ActivityShoppingListBinding binding;
+    private ArrayList<ShoppingItem> shoppingItems;
+    private ShoppingListAdapter shoppingListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityShoppingListBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        shoppingItems = new ArrayList<>();
+        shoppingListAdapter = new ShoppingListAdapter(new ArrayList<>(), new ShoppingListAdapter.OnItemInteractionListener() {
+            @Override
+            public void onCheckedChanged(ShoppingItem item, boolean isChecked) {
+                item.setChecked(isChecked);
+                new Thread(() -> SmartFridgeDatabase.getDatabase(getApplicationContext())
+                        .shoppingItemDAO()
+                        .update(item)).start();
+            }
+
+            @Override
+            public void onDelete(ShoppingItem item) {
+                new Thread(() -> {
+                    SmartFridgeDatabase.getDatabase(getApplicationContext())
+                            .shoppingItemDAO()
+                            .delete(item);
+
+                    runOnUiThread(() -> {
+                        shoppingItems.remove(item);
+                        shoppingListAdapter.notifyDataSetChanged();
+                    });
+                }).start();
+            }
+        });
+
     }
+
 
     public static Intent ShoppingListIntentFactory(Context context) {
         Intent intent = new Intent(context, ShoppingListActivity.class);
