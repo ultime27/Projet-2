2693065bbe209efdd731cb72a56e5a332d5a802f
cstremel98 <cstremel@csumb.edu>
2025-05-03 14:51:02 -0700

    Partial registerActivity. Firebase integration. Copied GoogleSignInActivity from Firebase (NEED INSTRUCTOR PERMISSION).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+        // Import the BoM for the Firebase platform
+        implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+
+        // Add the dependency for the Firebase Authentication library
+        // When using the BoM, you don't specify versions in Firebase library dependencies
+        implementation("com.google.firebase:firebase-auth")
+
+        // Also add the dependencies for the Credential Manager libraries and specify their versions
+        implementation("androidx.credentials:credentials:1.3.0")
+        implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+        implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
 }
 
 android {
@@ -8,7 +23,7 @@ android {
 
     defaultConfig {
         applicationId = "com.suchet.smartFridge"
-        minSdk = 28
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
