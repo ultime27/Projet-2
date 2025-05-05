9012882d1d64ea49981f74ae6479ae6a71297cef
MatthieusSuchet <109953186+MatthieusSuchet@users.noreply.github.com>
2025-05-04 21:51:17 -0700

    Update RecipeFragment.java

--- app/src/main/java/com/suchet/smartFridge/Recipie/RecipeFragment.java
+++ app/src/main/java/com/suchet/smartFridge/Recipie/RecipeFragment.java
@@ -40,7 +40,7 @@ public class RecipeFragment extends Fragment {
 
     private LocalDate selectedDate;
 
-    private ExecutorService executorService = Executors.newSingleThreadExecutor(); // Executor pour gérer les threads en arrière-plan
+    private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
     public static RecipeFragment newInstance(String name, String description, String instruction) {
         RecipeFragment fragment = new RecipeFragment();
@@ -113,7 +113,7 @@ public class RecipeFragment extends Fragment {
 
                         Meal newMeal = new Meal(mealName, selectedDate, recipeIngredients, userId);
 
-                        // Cette insertion doit maintenant se faire sur un thread d'arrière-plan
+                    
                         executorService.execute(() -> db.mealDAO().insert(newMeal));
 
                         requireActivity().runOnUiThread(() -> {
